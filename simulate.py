a7da9d83c38dce74887a8e8ce1554968e80cb408
Ryan Tuck <ryan@ryantuck.io>
2023-08-12 20:42:03 -0500

    models, not dicts

--- simulate.py
+++ simulate.py
@@ -1,30 +1,46 @@
 import json
 import math
 
+from pydantic import BaseModel
+
+class Velocity(BaseModel):
+    angle: float
+    speed: float
+
+class Position(BaseModel):
+    x: float
+    y: float
+
+class State(BaseModel):
+    time: float
+    position: Position
+    velocity: Velocity
 
 def is_finished(state):
     return False
 
 def _iterate_time(t, dt):
-    return round(t + dt, 1)
+    return round(t + dt,  1)
+
+def _iterate_speed(speed):
+    return 0.9 * speed
+
+def _iterate_angle(angle):
+    return angle
 
 def _iterate_velocity(v):
-    v['speed'] = v['speed'] * 0.9
+    v.speed = _iterate_speed(v.speed)
+    v.angle = _iterate_angle(v.angle)
     return v
 
-
 def _linear_motion(pos, v, dt):
-    angle = v['angle']
-    speed = v['speed']
-    x = pos['x']
-    y = pos['y']
 
-    distance = speed / dt
+    distance = v.speed / dt
 
-    x_f = x + distance * math.cos(angle)
-    y_f = y + distance * math.sin(angle)
+    x_f = pos.x + distance * math.cos(v.angle)
+    y_f = pos.y + distance * math.sin(v.angle)
 
-    return {'x': x_f, 'y': y_f}
+    return Position(x=x_f, y=y_f)
 
 
 def _iterate_position(pos, v, dt):
@@ -32,21 +48,28 @@ def _iterate_position(pos, v, dt):
 
 
 def iterate(state):
+
     dt = 0.1
-    state['t'] = _iterate_time(state['t'], dt)
-    state['v'] = _iterate_velocity(state['v'])
-    state['pos'] = _iterate_position(state['pos'], state['v'], dt)
+
+    state.time = _iterate_time(state.time, dt)
+    state.velocity = _iterate_velocity(state.velocity)
+    state.position = _iterate_position(state.position, state.velocity, dt)
+
     return state
 
 def simulate():
 
-    state = {'t': 0, 'v': {'speed': 8, 'angle': math.pi/4}, 'pos': {'x': 0, 'y': 0}}
+    state = State(
+        time=0,
+        position=Position(x=0,y=0),
+        velocity=Velocity(speed=8, angle=math.pi/4),
+    )
     n_steps = 100
 
     for i in range(n_steps):
         if is_finished(state):
             break
         state = iterate(state)
-        print(json.dumps(state))
+        print(json.dumps(state.dict()))
 
 simulate()
